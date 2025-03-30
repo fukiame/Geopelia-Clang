abad854c7e5b8c003a58179d5ad6504d18a981b2
fukiame <fukiame@proton.me>
2025-03-30 19:01:05 +0700

    avoid breaking bones
    Signed-off-by: fukiame <fukiame@proton.me>

--- build.sh
+++ build.sh
@@ -85,8 +85,8 @@ popd || exit
 llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
 clang_version="$("$HOME_DIR"/install/bin/clang --version | head -n1 | cut -d' ' -f4)"
 build_date="$(TZ=Asia/Ho_Chi_Minh date +"%Y-%m-%d")"
-tags="Apricot-Clang-$clang_version"
-file="Apricot-Clang-$clang_version.tar.gz"
+tags="Geopelia-Clang-$clang_version"
+file="Geopelia-Clang-$clang_version.tar.gz"
 
 # Get binutils version
 binutils_version=$(grep "LATEST_BINUTILS_RELEASE" build-binutils.py)
