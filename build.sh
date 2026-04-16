ea502f2a10c15a297dd6a9ba2c13990a43651ee7
fukiame <fukiame@proton.me>
2026-04-16 18:26:08 +0700

    build.sh: change clang vendor
    about time, aint we
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- build.sh
+++ build.sh
@@ -38,8 +38,8 @@ send_msg "gh $RUN_NUM: building LLVM"
     --shallow-clone \
     --targets AArch64 ARM X86 \
     --lto thin \
-    --clang-vendor-string "Geopelia" \
-    --lld-vendor-string "Gino"
+    --clang-vendor-string "Tsukuyomi" \
+    --lld-vendor-string "Kassen"
 
 # Check if the final clang binary exists or not
 for file in install/bin/clang-[1-9]*; do
